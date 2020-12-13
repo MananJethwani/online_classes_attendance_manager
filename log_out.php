b9b1b3788d8af1c948bd4c810f21bb1f2b22e8af
Manan Jethwani <50267986+MananJethwani@users.noreply.github.com>
2020-12-13 13:49:38 +0530

    Delete main_page.php
    Delete menu.php

--- log_out.php
+++
@@ -1,9 +0,0 @@
- <?php 
-//Destroy the session 
-session_start(); 
-session_unset(); 
-session_destroy(); 
-//Redirect to login page 
-header("location: login_form.php"); 
-exit(); 
-?>
--- menu.php
+++
@@ -1,8 +0,0 @@
-
- <?php 
-echo '<table width="1000"> 
-<tr><td><a href="main_page.php">Home</a></td> 
-<td align="right"><a href="log_out.php">Log Out </a></td> 
-</tr> 
-</table>'; 
-?>
